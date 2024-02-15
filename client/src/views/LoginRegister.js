45e60ffe6537bb290f7bdad1bb8e32de85bdf348
CheaFernandez <chelsea_anne_fernandez@dlsu.edu.ph>
2024-02-15 16:10:00 +0800

    Update LoginRegister.js

--- client/src/views/LoginRegister.js
+++ client/src/views/LoginRegister.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CryptoJS from 'crypto-js'
 import '../assets/css/LoginRegisterForm.css'
 
 import user_icon from '../assets/img/person.png'
@@ -6,6 +7,74 @@ import email_icon from '../assets/img/email.png'
 import password_icon from '../assets/img/password.png'
 import phone_icon from '../assets/img/phone.png'
 import dp_upload from '../assets/img/dp.png'
+;
+
+function validateEmail(email){
+    var validEmailRegex = new RegExp("/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/");
+
+    if (email.match(validEmailRegex)) {
+        return true;
+    }
+    return false;
+}
+
+function validatePassword(password){
+    var validPasswordRegex = new RegExp("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;")
+
+    if (password.match(validPasswordRegex)) {
+        return true;
+    }
+    return false;
+
+}
+
+function validatePhoneNumber(phonennumber){
+    var validPhoneNumberRegex = new RegExp("^\\+[1-9]{1}[0-9]{0,2}-[2-9]{1}[0-9]{2}-[2-9]{1}[0-9]{2}-[0-9]{4}$");
+
+    if (phonennumber.match(validPhoneNumberRegex)) {
+        return true;
+    }
+    return false;
+}
+function validateFullName(fullname){
+    var validFullName = new RegExp("/^[a-z ,.'-]+$/i");
+
+    if (fullname.match(validFullName)) {
+        return true;
+    }
+    return false;
+}
+
+function validateAndUploadDP(input){
+    var fileInput = input;
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+    var maxFileSize = 5 * 1024 * 1024; // 5MB 
+
+    // Check extension if the file is an image
+    if (!allowedExtensions.exec(fileInput)) {
+        alert('Invalid file type');
+        fileInput.value = '';
+        return false;
+    }
+    // Check file size
+    if (fileInput[0].files[0].size > maxFileSize) {
+        alert('File size is too large');
+        fileInput.value = '';
+        return false;
+    }
+    //Object validation
+    if (fileInput.length && fileInput[0].files && fileInput[0].files.length) {
+        var url = window.URL || window.webkitURL;
+        var image = new Image();
+        image.onload = function() {
+            alert('Valid Image');
+        };
+        image.onerror = function() {
+            alert('Invalid image');
+        };
+        image.src = url.createObjectURL(fileInput[0].files[0]);
+    }
+}
 
 function Login() {
     const [action, setAction] = useState('LOGIN');
@@ -21,6 +90,50 @@ function Login() {
             // Handle login
         } else {
             // Handle registration
+            // Validate the form
+            if (!fullname) {
+                alert('Please enter your full name');
+                return;
+            }
+            if (!phonennumber) {
+                alert('Please enter your phone number');
+                return;
+            }
+            if (!email) {
+                alert('Please enter your email');
+                return;
+            }
+            if (!password) {
+                alert('Please enter your password');
+                return;
+            }
+
+            if (!validateEmail(email)) {
+                alert('Please enter a valid email');
+                return;
+            }
+            if (!validatePassword(password)) {
+                alert('Please enter a valid password');
+                return;
+            }
+            if (!validatePhoneNumber(phonennumber)) {
+                alert('Please enter a valid phone number');
+                return;
+            }
+            if (!validateFullName(fullname)) {
+                alert('Please enter a valid full name');
+                return;
+            }
+
+            let user = {
+                "email": email,
+                "password": CryptoJS.AES.encrypt(password, 'key').toString(),
+                "number": phonennumber,
+                "fullname": fullname
+            }
+
+                    
+            console.log(`Registering user: ${fullname} - ${phonennumber} - ${email} - ${password}`);
         }
     }
 
@@ -30,12 +143,12 @@ function Login() {
                 <div className='text'>{action}</div>
                 <div className='underline'></div>
             </div>
-            <form className='form' onSubmit={handleSubmit}>
+            <form className='form' onSubmit={handleSubmit} encType='multipart/form-data'>
                 {action === "LOGIN" ? null : (
                     <div className='photo_input'>
                         <img src={dp_upload} alt='' />
                         <div className='upload_btn'>
-                            <input type='file'/>
+                            <input type='file' name="uploadPicture" accept="image/*" onChange="validateAndUploadDP(this);"/>
                         </div>
                     </div>
                 )}
