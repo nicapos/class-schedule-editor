9cbc25c1148383a9a2e5b6aec5396a6aa2f9498c
nicapos <67004893+nicapos@users.noreply.github.com>
2024-02-15 16:18:49 +0800

    feat: add db

---
+++ src/init.sql
@@ -0,0 +1,37 @@
+DO $$ BEGIN
+    CREATE TYPE ACCOUNT_TYPE AS ENUM('ADMIN', 'USER');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE IF NOT EXISTS users (
+    id          SERIAL PRIMARY KEY,
+    full_name   VARCHAR(255) NOT NULL,
+    email       VARCHAR(320) UNIQUE NOT NULL,
+    phone_num   VARCHAR(13),
+    photo_url   VARCHAR(255),
+    user_type   ACCOUNT_TYPE NOT NULL
+);
+
+DO $$ BEGIN
+    CREATE TYPE WEEK_DAY AS ENUM('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE IF NOT EXISTS class_items (
+    id          SERIAL PRIMARY KEY,
+    class_name  VARCHAR(255) NOT NULL,
+    day         WEEK_DAY NOT NULL,
+    start_time  VARCHAR(5) NOT NULL,
+    end_time    VARCHAR(5) NOT NULL,
+    location    VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS class_schedule (
+    id          SERIAL PRIMARY KEY,
+    user_id     INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    class_id INT,
+    FOREIGN KEY (class_id) REFERENCES class_items(id)
+);
