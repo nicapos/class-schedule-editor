5e6b2840bd927f78d5e0d821a59ef1c6cd5c000c
nicapos <67004893+nicapos@users.noreply.github.com>
2024-02-16 10:51:38 +0800

    Redirect root page logics
    Root page redirects to /app if logged-in, else redirects to /login

--- client/src/views/App.tsx
+++ client/src/views/App.tsx
@@ -1,24 +1,40 @@
-import React from "react";
-import logo from "../assets/img/logo.svg";
+import React, { useEffect } from "react";
 import "../assets/css/App.css";
+import { Loader2 as Spinner } from 'lucide-react';
+
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
+  function getCurrentUser() {
+    fetch("http://localhost:8080/api/me", {
+      method: "GET",
+      headers: {
+        'Accept': 'application/json',
+      },
+      credentials: "include",
+    })
+      .then(response => response.json())
+      .then((data) =>  {
+        if (!data.user) {
+          // No logged in user. Redirect back to login
+          navigate("/login");
+          return;
+        }
+        navigate("/app");
+        return;
+      });
+  }
+
+  useEffect(getCurrentUser, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="w-full h-screen flex flex-col items-center justify-center gap-4">
+        <Spinner className="h-32 w-32 animate-spin" />
+        <p>Loading app...</p>
+      </div>
     </div>
   );
 }
