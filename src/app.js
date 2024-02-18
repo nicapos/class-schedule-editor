d121451f790390014f93b1c058b3d9f4f04eadf2
nicapos <67004893+nicapos@users.noreply.github.com>
2024-02-18 11:05:39 +0800

    feat: add Typescript

--- src/app.js
+++ src/app.js
@@ -1,30 +1,31 @@
 const express = require("express");
-const { resolve } = require("path");
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 
 const routes = require("./routes");
 
 const app = express();
 
-app.set('trust proxy', 1);
-
-// serve static files generated from frontend build
-// app.use(express.static(resolve(__dirname, "..", "client", "build")));
+dotenv.config();
 
 // serve images saved to uploads
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
+app.set("trust proxy", 1);
 const corsMiddleware = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
   // Allow additional headers if needed
   // res.setHeader('Access-Control-Allow-Headers', 'Authorization');
 
   // Handle preflight requests
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
   }
@@ -41,17 +42,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // handles encrypted and signed cookies
-app.use(cookieSession({
-  name: 'cse-session',
-  keys: [process.env.SECRET_KEY],
-  maxAge: 24 * 60 * 60 * 1000, // 24 hours
-  httpOnly: true,
-  secure: false,
-}));
+app.use(
+  cookieSession({
+    name: "cse-session",
+    keys: [process.env.SECRET_KEY],
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    httpOnly: true,
+    secure: false,
+  })
+);
 
 // Add other middlewares here
 
 // Register routes
 app.use("/", routes);
 
-module.exports = app;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
