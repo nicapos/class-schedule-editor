e46ea8723463cb0236e74df07331b63323778d1b
CheaFernandez <chelsea_anne_fernandez@dlsu.edu.ph>
2024-03-19 22:41:54 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -63,8 +63,8 @@ db.sequelize.sync({ force: hasResetFlag })
 
 
 const PORT = process.env.PORT || 8080;
-// server.listen(PORT, () => {
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} (available at https://localhost:${PORT}/)`);
 
   // Show docs
