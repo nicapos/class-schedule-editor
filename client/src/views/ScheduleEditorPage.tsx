96e0329401d13fb3ccb40267f3271f329fc8dec8
nicapos <67004893+nicapos@users.noreply.github.com>
2024-03-19 12:25:23 +0800

    fix: data handling from updated backend

--- client/src/views/ScheduleEditorPage.tsx
+++ client/src/views/ScheduleEditorPage.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import CalendarPreview from "../components/CalendarPreview";
-import { Loader2 as Spinner } from 'lucide-react';
+import { Loader2 as Spinner } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-const placeholderAvatar = "https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg";
+const placeholderAvatar =
+  "https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg";
 
 export default function ScheduleEditorPage() {
   const [isLoading, setLoadingState] = useState<boolean>(true);
@@ -17,22 +18,22 @@ export default function ScheduleEditorPage() {
     fetch("http://localhost:8080/api/user/me", {
       method: "GET",
       headers: {
-        'Accept': 'application/json',
+        Accept: "application/json",
       },
       credentials: "include",
     })
-      .then(response => response.json())
-      .then((data) =>  {
+      .then((response) => response.json())
+      .then((data) => {
         if (!data.user) {
           // No logged in user. Redirect back to login
           navigate("/login");
           return;
         }
 
-        setName(data.user.full_name);
-        setIsAdmin(data.user.user_type === "ADMIN");
-        if (data.user.photo_url) {
-          setPhotoURL(data.user.photo_url);
+        setName(data.user.fullName);
+        setIsAdmin(data.user.userType === "ADMIN");
+        if (data.user.photoUrl) {
+          setPhotoURL(data.user.photoUrl);
         }
 
         setLoadingState(false);
@@ -67,21 +68,37 @@ export default function ScheduleEditorPage() {
         <CalendarPreview />
       </div>
 
-      {!isLoading && <div className="absolute top-2 right-2 flex gap-2">
-        {isAdmin && <button className="bg-gray-500 rounded-md text-sm" onClick={handleAdmin}>
-          Admin
-        </button>}
-        <button className="bg-gray-500 rounded-md text-sm" onClick={handleLogout}>
-          Log out
-        </button>
-      </div>}
+      {!isLoading && (
+        <div className="absolute top-2 right-2 flex gap-2">
+          {isAdmin && (
+            <button
+              className="bg-gray-500 rounded-md text-sm"
+              onClick={handleAdmin}
+            >
+              Admin
+            </button>
+          )}
+          <button
+            className="bg-gray-500 rounded-md text-sm"
+            onClick={handleLogout}
+          >
+            Log out
+          </button>
+        </div>
+      )}
 
-      {!isLoading && <div className="absolute top-2 left-2">
-        <span className="flex items-center gap-2">
-          <img src={photoURL} alt="Avatar" className="w-10 h-10 border border-black rounded-full" />
-          {name}
-        </span>
-      </div>}
+      {!isLoading && (
+        <div className="absolute top-2 left-2">
+          <span className="flex items-center gap-2">
+            <img
+              src={photoURL}
+              alt="Avatar"
+              className="w-10 h-10 border border-black rounded-full"
+            />
+            {name}
+          </span>
+        </div>
+      )}
     </div>
   );
 
@@ -90,7 +107,7 @@ export default function ScheduleEditorPage() {
       <Spinner className="h-32 w-32 animate-spin" />
       <p>Loading app...</p>
     </div>
-  )
+  );
 
   return isLoading ? renderLoading() : renderPage();
 }
