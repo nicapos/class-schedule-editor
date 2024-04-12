882f5247409722830c5c29e025501dc3ddfaa8df
CheaFernandez <chelsea_anne_fernandez@dlsu.edu.ph>
2024-04-13 04:38:41 +0800

    Create auth-services.js

---
+++ src/services/auth-services.js
@@ -0,0 +1,85 @@
+const https = require('https');
+const qs = require('qs');
+const axios = require('axios').default;
+
+function postRequest(url, data) {
+  return new Promise((resolve, reject) => {
+    const postData = qs.stringify(data);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    };
+
+    const req = https.request(url, options, (res) => {
+      let responseData = '';
+
+      res.on('data', (chunk) => {
+        responseData += chunk;
+      });
+
+      res.on('end', () => {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(JSON.parse(responseData));
+        } else {
+          reject(new Error(`HTTP request failed with status code ${res.statusCode}`));
+        }
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.write(postData);
+    req.end();
+  });
+}
+
+async function getGoogleOAuthTokens({ code }) {
+  const url = "https://oauth2.googleapis.com/token";
+
+  const values = {
+    code,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uri: process.env.REDIRECT_URI,
+    grant_type: "authorization_code",
+  };
+
+  try {
+    const res = await axios.post<GoogleTokensResult>(
+        url,
+        qs.stringify(values),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      return res.data;  
+  } catch (error) {
+    console.log(error);
+    throw new Error(error.message);
+  }
+}
+
+async function getGoogleUser({ id_token, access_token }) {
+    try {
+        const res = await axios.get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,{
+            headers: {
+              Authorization: `Bearer ${id_token}`,
+            },
+          }
+        );
+        return res.data;
+      } catch (error) {
+        log.error(error, "Error fetching Google user");
+        throw new Error(error.message);
+      }
+}
+
+module.exports = {getGoogleOAuthTokens, getGoogleUser};
